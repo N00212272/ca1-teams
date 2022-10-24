3937e8c4b7651bca9d60ed735e58fe4dea00154a
Kacper Agatowski <n00212272@iadt.ie>
2022-10-24 14:19:52 +0100

    created a seeder and updated

---
+++ database/seeders/TeamSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Team;
+
+class TeamSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //creates 50 different values
+        Team::factory()->times(50)->create();
+    }
+}
