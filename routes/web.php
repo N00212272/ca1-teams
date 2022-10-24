d0cc281c2cb81cbcc9bad24e4eabce502d7bf5c0
Kacper Agatowski <n00212272@iadt.ie>
2022-10-24 14:52:42 +0100

    created each function for the teamcontroller

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,9 @@
 <?php
-
+//Tells VSC where to find teamcontroller
+use App\Http\Controllers\TeamController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -20,5 +12,8 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+//This line create a route every function in the team resource controller
+Route::resource('/teams', TeamController::class)->middleware(['auth']);
 
 require __DIR__.'/auth.php';
