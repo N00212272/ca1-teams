ec52fb27aea6261f7150d6d608f0567b48aa693c
Kacper Agatowski <n00212272@iadt.ie>
2022-10-24 14:18:49 +0100

    created and updated team factory

---
+++ database/factories/TeamFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\team>
+ */
+class TeamFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //means it chooses this db and uses a fake word,text or name
+            'name' => $this->faker->word,
+            'user_id' => 1,
+            'category' => $this->faker->text(30),
+            'description' => $this->faker->text(200),
+            'team_image' => "public\images\team_placeholder.jpg",
+            'sponsor' => $this->faker->name
+        ];
+    }
+}
