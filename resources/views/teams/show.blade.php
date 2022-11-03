e0e06de169a50a3bd9caaea4dfee2d54a7c4fe4c
Kacper Agatowski <n00212272@iadt.ie>
2022-11-03 22:55:49 +0000

    created and modified the show page

--- resources/views/teams/show.blade.php
+++ resources/views/teams/show.blade.php
@@ -1,4 +1,3 @@
-
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -8,30 +7,33 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="flex">
+    
+                <div class="flex">
+                    {{-- brings in the updated and created at from db --}}
                 <p class="opacity-70">
                     <strong>Created: </strong> {{ $team->created_at->diffForHumans() }}
                 </p>
-                <p class="opacity-70 ml-8">
+              
+                  <p class="opacity-70 ml-8">
                     <strong>Updated at: </strong> {{ $team->updated_at->diffForHumans() }}
                 </p>
-                <a href="{{ route('teams.edit', $team) }}" class="btn-link ml-auto">Edit Team</a>
-                <form action="{{ route('teams.destroy', $team) }}" method="post">
-                    @method('delete')
-                    @csrf
-                    <button type="submit" class="btn btn-danger ml-4" onclick="return confirm('Are you sure you wish to delete this note?')">Delete Note</button>
-            </div>
-            <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
-                <h2 class="font-bold text-4xl">
-                    {{ $team->title }}
-                </h2>
-                <p class="mt-6 whitespace-">{{$team->text}}</p>
-            </div>
-            <tr>
-            <td rowspan="6">
-                <img src="{{('storage/images/' . $team->team_image) }}" width="150" />
-            </td>
-            <tr>
+
+                </div>
+                    <p class="mt-2">
+                   {{ Str::limit($team->name, 200) }}
+                    </p>
+             
+                     <p class="mt-2 whitespace-pre-wrap">{{($team->category) }}</p>
+                
+                {{-- this class wraps the text to make it look nicer --}}
+                    <p class="mt-2 whitespace-pre-wrap">{{ $team->description }} </p>
+
+                 <img src="{{ asset('storage/images/' . $team->team_image) }}" width="150">  
+
+               
+                {{-- If there are no teams and its 'empty' it will show this message --}}
+            
+         
         </div>
     </div>
 </x-app-layout>
