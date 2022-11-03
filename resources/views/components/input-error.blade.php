b689d9c344e3ec8cb9694c436f30823fb1bd4d09
Kacper Agatowski <n00212272@iadt.ie>
2022-11-03 22:02:01 +0000

    added in comments

--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -1,5 +1,5 @@
 @props(['messages'])
-
+{{-- for each message which where the field is required or doesnt match the requirements add an error message --}}
 @if ($messages)
     <ul {{ $attributes->merge(['class' => 'text-sm text-red-600 space-y-1']) }}>
         @foreach ((array) $messages as $message)
