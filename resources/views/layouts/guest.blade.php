3c67bebc5bc5a0786442c8396c6d5eb9d60b42f8
Kacper Agatowski <n00212272@iadt.ie>
2022-11-03 22:03:13 +0000

    added stylesheet

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,6 +10,10 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
+
+        {{-- Styles --}}
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
