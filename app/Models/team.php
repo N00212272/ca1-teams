5844655ebe9c54c9cc03bb53aed18e86d658b3a4
Kacper Agatowski <n00212272@iadt.ie>
2022-11-03 21:57:45 +0000

    added guarded to mass assign all fields

--- app/Models/team.php
+++ app/Models/team.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class team extends Model
 {
     use HasFactory;
+    // indicating that these fields are guarded
+    // all fields are mass assigned
+    protected $guarded = [];
+    // where if i would want them to be fillable i would change the guarded to fillable like this
+    //  protected $fillable = [''];
 }
