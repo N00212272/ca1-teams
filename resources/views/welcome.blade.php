3fea07f3e3edc5b5df0de4ad6e402b98f2c9497d
Kacper Agatowski <n00212272@iadt.ie>
2022-11-03 22:01:14 +0000

    changed some styles in welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,12 @@
 
         <title>Teams</title>
 
+         {{-- Styles --}}
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
@@ -24,22 +30,22 @@
         </style> --}}
     </head>
     <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+        <div class="relative flex items-top justify-center min-h-screen bg-gray-100  sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ url('/teams') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">teams</a>
+                        <a href="{{ url('/teams') }}" class="text-sm text-gray-700 underline">teams</a>
                     @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700  underline">Log in</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700  underline">Register</a>
                         @endif
                     @endauth
                 </div>
             @endif
             {{-- Simple h1 text which says teams --}}
-             <h1 class="text-5xl-bold-capitalised">Teams</h1>
+             <h1 class="teams">Teams</h1>
 
              {{-- Removed all these irrelvant things --}}
 
@@ -129,11 +135,11 @@
                         </div>
                     </div> --}}
 
-                    <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
+                    {{-- <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </div>
                 </div>
-            </div>
+            </div> --}}
         </div>
     </body>
 </html>
