7e8f7349051a2374e8fecb24772c908f9a36508d
Kacper Agatowski <n00212272@iadt.ie>
2022-11-04 11:17:44 +0000

    when clicked edit brings to edit view

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -104,9 +104,16 @@ class TeamController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Team $team)
     {
-        //
+          //if statement which doesnt allow another user to edit other teams
+          if($team->user_id != Auth::id()){
+            return abort(403);
+        }
+        //This shows the information of the chosen id under that specific user
+        return view('teams.edit')->with('team',$team);
+
+     
     }
 
     /**
@@ -116,9 +123,41 @@ class TeamController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Team $team)
     {
-        //
+
+             //creating variable for image and its extension
+             $team_image = $request->file('team_image');
+             $extension = $team_image->getClientOriginalExtension();
+     
+             //file name has to be unique so i added in this for a unqiue name so it can be definitly be found
+             $filename= date('Y-m-d-His') . '_' . $request->input('title') . '.' . $extension;
+             
+             // store the file $team_image in /public/images, and name is $filename
+             $path = $team_image->storeAs('public/images', $filename);
+
+         //if statement which doesnt allow another user to update other teams
+         if($team->user_id != Auth::id()){
+            return abort(403);
+        }
+
+        // dd($request);
+        //validates updated database
+        $request->validate([
+            'name' => 'required|max:120',
+            'category' => 'required',
+            'description' => 'required',
+            'team_image' => 'required'
+        ]);
+            //updates the variables in database
+        $team->update([
+            'name' => $request->name,
+            'category' => $request->category,
+            'description' => $request->description,
+            'team_image' => $filename
+        ]);
+
+        return to_route('teams.show', $team);
     }
 
     /**
