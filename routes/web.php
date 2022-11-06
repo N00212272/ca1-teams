03a5f345ce208039fc38476125163d0d747d222e
Kacper Agatowski <n00212272@iadt.ie>
2022-11-06 22:38:40 +0000

    The search statement

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 //Tells VSC where to find teamcontroller
 use App\Http\Controllers\TeamController;
 use Illuminate\Support\Facades\Route;
+use App\Models\Team;
+use Illuminate\Support\Facades\Input;
+
 
 
 
@@ -15,5 +18,19 @@ Route::get('/dashboard', function () {
 
 //This line create a route every function in the team resource controller
 Route::resource('/teams', TeamController::class)->middleware(['auth']);
+
+Route::post ( '/search', function () {
+	$q = Request::input ( 'q' );
+
+	if($q != ""){
+		$team = Team::where ( 'name', 'LIKE', '%' . $q . '%' )->get ();
+		if (count ( $team ) > 0)
+			return view ( 'welcome' )->withDetails ( $team )->withQuery ( $q );
+		else
+			return view ( 'welcome' )->withMessage ( 'No Details found. Try to search again !' );
+	}
+	
+	return view ( 'search-functionality-in-laravel/welcome' )->withMessage ( 'No Details found. Try to search again !' );
+} );
 
 require __DIR__.'/auth.php';
