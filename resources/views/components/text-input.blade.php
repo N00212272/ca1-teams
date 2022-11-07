2aff7dc1c7d7c8a554313169aea9152e1b5d53cd
Kacper Agatowski <n00212272@iadt.ie>
2022-11-07 14:50:54 +0000

    fixed name validation

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,9 @@
-@props(['disabled' => false])
+@props(['disabled' => false, 'field'=>'', 'value'=>'' ])
 
 <input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50']) !!}>
+
+@error($field)
+
+    <div class="text-red-600 text-sm">{{$message}}</div>
+
+@enderror
