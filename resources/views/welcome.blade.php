96b35bb898c69809d429dd43f7a228dbd45c68e8
Kacper Agatowski <n00212272@iadt.ie>
2022-11-18 11:01:06 +0000

    changed route to home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,7 +34,7 @@
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ url('/teams') }}" class="text-sm text-gray-700 underline">teams</a>
+                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">teams</a>
                     @else
                         <a href="{{ route('login') }}" class="text-sm text-gray-700  underline">Log in</a>
 
