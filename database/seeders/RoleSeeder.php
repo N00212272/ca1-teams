75f3c4792fec03750a8fa5613aa4499e9fe174c0
Kacper Agatowski <n00212272@iadt.ie>
2022-11-18 11:00:18 +0000

    created role seeder

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role_admin = new Role();
+        $role_admin->name = 'admin';
+        $role_admin->description = 'An Admininstrator user';
+        $role_admin->save();
+
+        $role_user = new Role();
+        $role_user->name = 'user';
+        $role_user->description = 'An ordinary user';
+        $role_user->save();
+    }
+}
