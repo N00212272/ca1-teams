53ff7f89252cee8f65dd6078d25bd3b9a0d13f2a
Kacper Agatowski <n00212272@iadt.ie>
2022-11-18 10:58:28 +0000

    added roles function

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,29 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function roles()
+    {
+        return $this->belongsToMany('App\Models\Role', 'user_role');
+    }
+
+
+    public function authorizeRoles($roles)
+    {
+        if(is_array($roles)){
+            return $this->hasAnyRole($roles) ||
+            abort (401, 'This action is unauthorzed');
+        }
+        return $this->hasRole($roles) ||
+        abort(401, 'This action is unauthorized');
+    }
+
+    public function hasRole($role)
+    {
+        return null !== $this->roles()->where('name', $role)->first();
+    }
+
+    public function hasAnyRole($roles)
+    {
+        return null !== $this->roles()->whereIn('name', $roles)->first();
+    }
 }
