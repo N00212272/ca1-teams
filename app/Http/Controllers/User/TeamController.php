84dc6c20a5b2aabdc25cf085c766041e2eaf43d7
Kacper Agatowski <n00212272@iadt.ie>
2022-11-18 10:57:56 +0000

    moved to user folder and changed values to user

---
+++ app/Http/Controllers/User/TeamController.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+
+use App\Http\Controllers\Controller;
+use App\Models\Team;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+
+
+class TeamController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        $teams = Team::paginate(10);
+    //fetch Teams in order of when they were last update - latest updated returned first
+    //paginate displays whatever amount is placed e.g i have 10
+    // $teams = Team::where('user_id', Auth::id())->latest('updated_at')->paginate(10);
+    //dd($teams);
+
+    return view('user.teams.index')->with('teams', $teams);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //shows the create view from create.blade.php
+        // return view('teams.create');
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        
+        // //   dd($request);
+        // $request->validate([
+        //     'name' => 'required|max:120',
+        //     'category' => 'required',
+        //     'description' => 'required',
+        //     'team_image' => 'required'
+        // ]);
+        // //creating variable for image and its extension
+        // $team_image = $request->file('team_image');
+        // $extension = $team_image->getClientOriginalExtension();
+
+        // //file name has to be unique so i added in this for a unqiue name so it can be definitly be found
+        // $filename= date('Y-m-d-His') . '_' . $request->input('title') . '.' . $extension;
+        
+        // // store the file $team_image in /public/images, and name is $filename
+        // $path = $team_image->storeAs('public/images', $filename);
+
+        // // Creating a new team function
+        //     $team = new Team;
+
+        //     $team->uuid = Str::uuid();
+        //     $team->user_id =Auth::id();
+        //     $team->name =$request->name;
+        //     $team->category =$request->category;
+        //     $team->description =$request->description;
+        //     $team->team_image =$filename;
+            
+
+        //     $team->save();
+            
+      
+
+        // return to_route('teams.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Team $team)
+    {
+        //if statement which doesnt allow another user to view other teams
+        // if($team->user_id != Auth::id()){
+        //     return abort(403);
+        // }
+        //This shows the information of the chosen id under that specific user
+        // $team = Team::where('uuid',$uuid)->where('user_id',Auth::id())->first();
+        return view('user.teams.show')->with('team',$team);
+
+    }  
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Team $team)
+    {
+         //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Team $team)
+    {
+//
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Team $team)
+    {
+     //
+    }
+}
+
+    
