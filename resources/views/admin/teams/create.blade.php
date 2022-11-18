d85b1463a66dc897ea66c20b19cde3dd9a55035a
Kacper Agatowski <n00212272@iadt.ie>
2022-11-18 11:01:23 +0000

    created admin create

---
+++ resources/views/admin/teams/create.blade.php
@@ -0,0 +1,56 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Teams') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
+
+                <form action="{{ route('admin.teams.store') }}" method="post" enctype="multipart/form-data">
+                    @csrf
+                    {{-- Name input --}}
+                    <x-textarea
+                        type="text"
+                        name="name"
+                        {{-- FIELD connects to the error messages --}}
+                        field="name"
+                        placeholder="name"
+                        class="w-full"
+                        autocomplete="off"
+                        {{-- Value keeps the old value if an error occurs if was correct --}}
+                        :value="@old('name')"></x-textarea>
+                     
+                    {{-- category input --}}
+                    <x-textarea
+                        type="text"
+                        name="category"
+                        field="category"
+                        placeholder="category"
+                        class="w-full"
+                        autocomplete="off"
+                        :value="@old('category')"></x-textarea>
+                      <x-textarea
+                        type="text"
+                        name="description"
+                        rows="10"
+                        field="description"
+                        placeholder="Start typing here..."
+                        class="w-full mt-6"
+                        :value="@old('description')"></x-textarea>
+                        <input
+                        type="file"
+                        name="team_image"
+                        placeholder="Team Cover"
+                        class="w-full mt-6"
+                        field="team_image"
+                        >
+
+                    <x-primary-button class="mt-6">Save Team</x-primary-button>
+                </form>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
