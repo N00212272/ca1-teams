dad325bfb29497eee004ea762c50f23c93203dfc
Kacper Agatowski <n00212272@iadt.ie>
2022-11-18 10:58:10 +0000

    created role model

---
+++ app/Models/role.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class role extends Model
+{
+    use HasFactory;
+
+
+
+    //this function allows you to do $role->users which will return all users for that role
+    public function users()
+    {
+        return $this->belongsToMany('App\Models\User', 'user_role');
+    }
+    
+}
+
