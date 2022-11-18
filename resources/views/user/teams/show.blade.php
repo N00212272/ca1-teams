1f035a2d8ef89d711248fb910c086d1e2535968a
Kacper Agatowski <n00212272@iadt.ie>
2022-11-18 11:03:29 +0000

    created user show

---
+++ resources/views/user/teams/show.blade.php
@@ -0,0 +1,37 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Teams') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    
+                <div class="flex">
+                    {{-- brings in the updated and created at from db --}}
+                <p class="opacity-70">
+                    <strong>Created: </strong> {{ $team->created_at->diffForHumans() }}
+                </p>
+              
+                  <p class="opacity-70 ml-8">
+                    <strong>Updated at: </strong> {{ $team->updated_at->diffForHumans() }}
+                </p>
+               </div>
+                    <p class="mt-2">
+                   {{ Str::limit($team->name, 200) }}
+                    </p>
+             
+                     <p class="mt-2 whitespace-pre-wrap">{{($team->category) }}</p>
+                
+                {{-- this class wraps the text to make it look nicer --}}
+                    <p class="mt-2 whitespace-pre-wrap">{{ $team->description }} </p>
+
+                 <img src="{{ asset('storage/images/' . $team->team_image) }}" width="150">  
+
+              
+            
+         
+        </div>
+    </div>
+</x-app-layout>
