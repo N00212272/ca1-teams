727d26fabdcd459a254717855bf260da01d6e50d
Kacper Agatowski <n00212272@iadt.ie>
2022-11-21 15:07:07 +0000

    created owner fk migration

---
+++ database/migrations/2022_11_18_111919_add_owner_id_to_teams_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('teams', function (Blueprint $table) {
+            $table->unsignedBigInteger('owner_id');
+            $table->foreign('owner_id')->references('id')->on('owners')->onUpdate('cascade')->onDelete('restrict');
+       
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('teams', function (Blueprint $table) {
+            $table->dropForeign(['owner_id']);
+            $table->dropColumn('owner_id');
+        });
+    }
+};
