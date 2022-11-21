e8f94192115f83b298e3ae0b95a8954b582a2e24
Kacper Agatowski <n00212272@iadt.ie>
2022-11-21 15:07:30 +0000

    created owner seeder

---
+++ database/seeders/OwnerSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Owner;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class OwnerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Owner::factory()
+        ->times(3)
+        ->hasTeams(4)
+        ->create();
+    }
+}
