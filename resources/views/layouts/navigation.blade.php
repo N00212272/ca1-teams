94b5f7273e28359c36d1ac8267b26d324f8d914d
Kacper Agatowski <n00212272@iadt.ie>
2022-11-29 20:12:04 +0000

    created a new navigation for owner

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,8 +16,14 @@
                         {{ __('Teams') }}
                     </x-nav-link>
                 </div>
-            </div>
-
+           
+              {{-- Owner links --}}
+                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('home.owner.index')" :active="request()->routeIs('home.owner.index')">
+                        {{ __('Owners') }}
+                    </x-nav-link>
+                </div>
+        </div>
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 <x-dropdown text-align="right" width="48">
