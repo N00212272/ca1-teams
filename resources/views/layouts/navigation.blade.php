e3238cc62a47f555485c73fb8aae2c436bdc8a1c
Kacper Agatowski <n00212272@iadt.ie>
2022-12-07 17:32:09 +0000

    created a nav for sponsors

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -23,6 +23,12 @@
                         {{ __('Owners') }}
                     </x-nav-link>
                 </div>
+
+                  <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('home.sponsor.index')" :active="request()->routeIs('home.sponsor.index')">
+                        {{ __('Sponsors') }}
+                    </x-nav-link>
+                </div>
         </div>
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
