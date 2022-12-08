214ef86ca5d067ab7743c1c4a1d41c51194ecab8
Kacper Agatowski <n00212272@iadt.ie>
2022-12-08 22:21:58 +0000

    user sponsor controller created

---
+++ app/Http/Controllers/User/SponsorController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Sponsor;
+use Illuminate\Support\Facades\Auth;
+
+
+class SponsorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $user = Auth::user();
+        $user->authorizeRoles('user');
+
+       $sponsors = Sponsor::all();
+
+        return view('user.sponsors.index')->with('sponsors', $sponsors);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        // 
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // 
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(sponsor $sponsor)
+    {
+        $user = Auth::user();
+        $user->authorizeRoles('user');
+
+        if(!Auth::id()) {
+           return abort(403);
+         }
+
+
+        return view('user.sponsors.show')->with('sponsor', $sponsor);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        // 
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Sponsor $sponsor)
+    {
+        // 
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+    //    
+    }
+}
